0e082b1087d3b34fd431affc42acf86d57df5b48
Brad Kilkenney <bkilkenney@gmail.com>
2016-11-13 12:22:32 -0500

    Add SoloLearn class and methods with parameters

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -12,31 +12,64 @@ namespace Classes
         {
 
             Animal newAnimal = new Animal();
-            newAnimal.Move();
-            newAnimal.MakeNoise();
+            //newAnimal.Move();
+            //newAnimal.MakeNoise();
             newAnimal.Age = 5;
 
-            Console.ReadLine();
+            //Console.ReadLine();
 
             Dog Sami = new Dog();
             Sami.Age = 13;
             Sami.NumOfLegs = 4;
             
 
-            Console.ReadLine();
+            //Console.ReadLine();
 
 
             Dog Rufus = new Dog();
             Rufus.Age = 5;
-            Rufus.Move();
-            Rufus.MakeNoise();
+            //Rufus.Move();
+            //Rufus.MakeNoise();
 
+            //Console.ReadLine();
+
+            Console.WriteLine(DateTime.Now);
+
+            Console.ReadLine();
+
+            SoloLearn sl = new SoloLearn();
+
+            SoloLearn s2 = new SoloLearn();
+
+            sl.TimesTwo(36);
+
+            Console.ReadLine();
+
+            Console.WriteLine("The sum of the parameters is " + s2.Sum(34, 43));
+
+            Console.ReadLine();
+
+
+            Console.WriteLine(Pow(3, 4));
+
+            Console.WriteLine(Pow(6));
 
             
+
         }
 
 
-       
+        static int Pow(int x, int y = 2)
+        {
+            int result = 1;
+            for (int i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+            return result;
+        }
+
+
     }
 
 
---
+++ Classes/SoloLearn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class SoloLearn
+    {
+        //Class is just to play with sololearn tutorials
+
+        public void TimesTwo(int x)
+        {
+            Console.WriteLine("The number you entered multiplied by 2 is: " + x * 2);
+        }
+
+
+        public int Sum(int c, int d)
+        {
+            int f = c + d;
+            return f;
+        }
+
+        
+       
+    }
+}
